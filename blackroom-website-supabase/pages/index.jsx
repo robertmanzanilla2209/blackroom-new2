701010839f6f56dc1984315ce3c63b0170f080a0
robertmanzanilla2209 <rob_alex245@hotmail.com>
2025-04-24 01:20:29 -0400

    Delete blackroom-website-supabase directory

--- blackroom-website-supabase/pages/index.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-black text-white flex flex-col justify-center items-center">
-      <h1 className="text-4xl text-blue-500 font-bold">Bienvenido a Black Room</h1>
-      <p className="mt-4 text-lg">Explora los eventos, la música y regístrate</p>
-    </div>
-  );
-}
